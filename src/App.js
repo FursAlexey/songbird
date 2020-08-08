1041e5fa36ddc0824fd39d79ab6a2994e625e24f
Alexey Furs <wiemrid@gmail.com>
2020-08-08 13:21:34 +0300

    feat: add navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import Navigation from './components/Navigation';
+
 function App() {
   return (
     <div className="App">
+      <header>
+        <Navigation />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Navigation.scss";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Разминка</a></li>
+        <li><a href="#">Воробьиные</a></li>
+        <li><a href="#">Лесные птицы</a></li>
+        <li><a href="#">Певчие птицы</a></li>
+        <li><a href="#">Хищные птицы</a></li>
+        <li><a href="#">Морские птицы</a></li>
+      </ul>
+    </nav>
+  );
+}
