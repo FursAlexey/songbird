4c1b32937f02103428a6ce982d0e0cd66a4f2cef
Alexey Furs <wiemrid@gmail.com>
2020-08-08 13:42:02 +0300

    feat: add logo and score

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 import Navigation from './components/Navigation';
 
 function App() {
+  const [score, setScore] = useState(0);
+
   return (
     <div className="App">
       <header>
+        <div className="logo_score">
+          <img src="./svg/logo.svg" width="200" alt="song_bird"/>
+          <h5>Score: {score}</h5>
+        </div>
         <Navigation />
       </header>
       <main>
